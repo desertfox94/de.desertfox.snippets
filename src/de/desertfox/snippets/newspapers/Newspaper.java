3ecc81e8ef180e33596965dd47172ec57195f3d0
ddonges <ddonges@vmEntwicklerDD>
2018-05-24 16:01:40 +0200

    german newspaper loader
    Change-Id: I59a3ce608f1823dab51a3c80b8e6b10a9f08135b

---
+++ src/de/desertfox/snippets/newspapers/Newspaper.java
@@ -0,0 +1,10 @@
+package fhl.mib.seminar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Newspaper {
+	String wikiLink;
+	String name;
+	List<String> homepage = new ArrayList<>();
+}
